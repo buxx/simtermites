f91412bcf5ec28b28f633ab3b1438050617d8d55
Sevajol Bastien <sevajol.bastien@gmail.com>
2014-08-20 22:27:43 +0200

    update factory sandbox

--- sandbox/factory/factory.py
+++ sandbox/factory/factory.py
@@ -3,14 +3,24 @@ class Factory:
         def __init__(self):
             self._extend_registry = {}
             self._class_registry = {}
-        def extend(self, object_name, theclass):
+        def extend(self, object_name, extend_class, parent_class):
             if object_name not in self._extend_registry:
                 self._extend_registry[object_name] = []
-            self._extend_registry[object_name].append(theclass)
+            try:
+              index_of_existent_extend = self._extend_registry[object_name].index(parent_class)
+              self._extend_registry[object_name][index_of_existent_extend] = extend_class
+            except ValueError:
+              self._extend_registry[object_name].append(extend_class)
+            #try:
+            #  index = self._extend_registry[object_name].index(theclass.__bases__[0])
+            #  self._extend_registry[object_name][0] = theclass
+            #except ValueError:
+            #  self._extend_registry[object_name].append(theclass)
         
         def get_class(self, object_name):
             if object_name not in self._class_registry:
                 # Construction d'une classe etendue a partir des sous classes existantes
+                print self._extend_registry[object_name]
                 self._class_registry[object_name] = type('Final'+object_name, tuple(self._extend_registry[object_name]), {})
             return self._class_registry[object_name]
             
