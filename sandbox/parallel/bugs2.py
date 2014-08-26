949ba4060daf0ece45c03314906d3cd314f5c18f
Bastien Sevajol <bastien.sevajol@xsalto.com>
2014-08-26 09:07:40 +0200

    Process Manager: Objet pour processus persistant et objet pour processus ephemere

--- sandbox/parallel/bugs2.py
+++ sandbox/parallel/bugs2.py
@@ -2,7 +2,7 @@ import sys
 
 from multiprocessing import Process, Pipe
 from multiprocessing.connection import wait
-from processmanager import ProcessManager
+from processmanager import KeepedAliveProcessManager, ProcessManager
 from random import choice
 import timeit
 
@@ -25,49 +25,50 @@ def run_worker(bugs):
   worker.action_them()
   return worker.bugs
 
-n=1000
+n=100000
 bugs = []
 for i in range(n):   
   bugs.append(Bug(i))
 
 def compute_keep_alive(bugs, nb_process=1, repeat=1):
-  process_manager = ProcessManager(nb_process, run_worker)
+  process_manager = KeepedAliveProcessManager(nb_process, run_worker)
   for i in range(repeat):
     process_manager.get_their_work(bugs)
   process_manager.stop()
 
 def compute_recreate(bugs, nb_process=1, repeat=1):
+  process_manager = ProcessManager(nb_process, run_worker)
   for i in range(repeat):
-    process_manager = ProcessManager(nb_process, run_worker)
     process_manager.get_their_work(bugs)
-    process_manager.stop()
+
+max_process = '4'
 
 print('keep: 1 processs, 01 cycles: ', timeit.timeit('compute_keep_alive(bugs, nb_process=1, repeat=1)',\
-                                                     number=20,\
+                                                     number=1,\
                                                      setup='from __main__ import bugs,Bug,SimpleWorker,compute_keep_alive'))
 print('recr: 1 processs, 01 cycles: ', timeit.timeit('compute_recreate(bugs, nb_process=1, repeat=1)',\
-                                                     number=20,\
+                                                     number=1,\
                                                      setup='from __main__ import bugs,Bug,SimpleWorker,compute_recreate'))
 
 print('keep: 1 processs, 25 cycles: ', timeit.timeit('compute_keep_alive(bugs, nb_process=1, repeat=25)',\
-                                                     number=20,\
+                                                     number=1,\
                                                      setup='from __main__ import bugs,Bug,SimpleWorker,compute_keep_alive'))
 print('recr: 1 processs, 25 cycles: ', timeit.timeit('compute_recreate(bugs, nb_process=1, repeat=25)',\
-                                                     number=20,\
+                                                     number=1,\
                                                      setup='from __main__ import bugs,Bug,SimpleWorker,compute_recreate'))
 
-print('keep: 2 processs, 01 cycles: ', timeit.timeit('compute_keep_alive(bugs, nb_process=2, repeat=1)',\
-                                                     number=20,\
+print('keep: '+max_process+' processs, 01 cycles: ', timeit.timeit('compute_keep_alive(bugs, nb_process='+max_process+', repeat=1)',\
+                                                     number=1,\
                                                      setup='from __main__ import bugs,Bug,SimpleWorker,compute_keep_alive'))
-print('recr: 2 processs, 01 cycles: ', timeit.timeit('compute_recreate(bugs, nb_process=2, repeat=1)',\
-                                                     number=20,\
+print('recr: '+max_process+' processs, 01 cycles: ', timeit.timeit('compute_recreate(bugs, nb_process='+max_process+', repeat=1)',\
+                                                     number=1,\
                                                      setup='from __main__ import bugs,Bug,SimpleWorker,compute_recreate'))
 
-print('keep: 2 processs, 25 cycles: ', timeit.timeit('compute_keep_alive(bugs, nb_process=2, repeat=25)',\
-                                                     number=20,\
+print('keep: '+max_process+' processs, 25 cycles: ', timeit.timeit('compute_keep_alive(bugs, nb_process='+max_process+', repeat=25)',\
+                                                     number=1,\
                                                      setup='from __main__ import bugs,Bug,SimpleWorker,compute_keep_alive'))
-print('recr: 2 processs, 25 cycles: ', timeit.timeit('compute_recreate(bugs, nb_process=2, repeat=25)',\
-                                                     number=20,\
+print('recr: '+max_process+' processs, 25 cycles: ', timeit.timeit('compute_recreate(bugs, nb_process='+max_process+', repeat=25)',\
+                                                     number=1,\
                                                      setup='from __main__ import bugs,Bug,SimpleWorker,compute_recreate'))
 
 
